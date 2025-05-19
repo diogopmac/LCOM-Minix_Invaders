e3bf739c29bdaaee0e9e0b7cb8fe9c934acd878b
diogo <diogo.campeao@gmail.com>
2025-05-19 20:00:44 +0100

    Revert "Move mouse improvements + Double buffering, first iteration"
    This reverts commit 6dfce032aaed1be8a94f4fabac40a6483c8b657d.

--- proj/src/controllers/video/video.h
+++ proj/src/controllers/video/video.h
@@ -5,12 +5,6 @@
 
 int (video_set_mode)(uint16_t mode);
 int (video_map_memory)(uint16_t mode);
-void (video_swap_buffer)();
-
-unsigned int (get_vertical_resolution)();
-unsigned int (get_horizontal_resolution)();
-unsigned int (get_bits_per_pixel)();
-
 
 int (vg_draw_pixel)(uint16_t x, uint16_t y, uint32_t color);
 int (vg_draw_hline)(uint16_t x, uint16_t y, uint16_t len, uint32_t color);
